30369d0105827d45f2b942e631390251c2f18e6c
mukasine marie claire <mclaremukasine@gmail.com>
2019-04-05 12:48:21 +0200

    adding class profileUploadForm in forms.py

--- awwa/models.py
+++ awwa/models.py
@@ -93,3 +93,7 @@ class Votes(models.Model):
     usability = models.IntegerField(choices=list(zip(range(2, 10), range(2, 10))))
     content = models.IntegerField(choices=list(zip(range(2, 10), range(2, 10))))
 
+class MoringaMerch(models.Model):
+    name = models.CharField(max_length=40)
+    description = models.TextField()
+    price = models.DecimalField(decimal_places=2, max_digits=20)
